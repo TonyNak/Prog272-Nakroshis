5018ed3c12eedfa135d69ffc25794ec247c293eb
unknown <Tony@TONY-LP.HomeCenter.NakroshisWest.com>
2014-01-16 16:11:56 -0800

    Moving the Delete12 project tothe repository.

---
+++ Delete12/index.js
@@ -0,0 +1,18 @@
+/**
+ * @author Tony
+ */
+
+var foo = {
+	firstName: 'able',
+	lastName: 'Thomas',
+	func: function() {
+		console.log('func called');
+	}
+};
+
+function goober() {
+	
+}
+
+foo.func();
+
